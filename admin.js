9938b11919dfe62f4337dacdc49d2bb0592f4873
ferdboy <77190186+ferdboy@users.noreply.github.com>
2021-01-13 00:48:18 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,2 +1,2 @@
-const ownerNumber = ["6282169369877@s.whatsapp.net"] // replace this with your number
-const adminbotnumber = ["6282169369877@s.whatsapp.net"]
+const owner = ["6282169369877@s.whatsapp.net"] // replace this with your number
+const adminbot = ["6282169369877@s.whatsapp.net"]
